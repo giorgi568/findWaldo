a0daecefb86c9f13af0168868d7a49b78315c765
giorgi568 <mateshvili02@mail.ru>
2024-04-27 22:15:54 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <h1>Lets Find Waldo</h1>
+       <div className="leaderboard">
+        
+       </div>
+      </div>
+    </>
+  )
+}
+
+export default App
